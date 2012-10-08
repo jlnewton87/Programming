6b83e374636eb144999e9d8de98d0002eb6d45fc
Josh Newton <jnewton@cmiresearch.com>
2012-10-08 12:23:04 -0400

    Adding Hotel Occupancy Assignment with DataGridView instead of ListBox

---
+++ C#/HotelAssignmentWithGridView/Joshua_Newton_Assignment3/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Joshua_Newton_Assignment3")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Dracosoft RnD")> 
+<Assembly: AssemblyProduct("Joshua_Newton_Assignment3")> 
+<Assembly: AssemblyCopyright("Copyright © Dracosoft RnD 2012")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("9ca31006-8e76-426b-be23-7a45539f8c6f")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
