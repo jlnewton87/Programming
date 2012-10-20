b536d43eeeb578e7169884c28ed4abaa3107d536
Joshua Newton <jnewton@cmiresearch.com>
2012-10-20 14:05:02 -0400

    Adding RemoveTags project which removes the tags/keywords from all files in a selected directory

---
+++ C#/RemoveTags/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace RemoveTags
+{
+   partial class Form1
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.fbdTaggedDocument = new System.Windows.Forms.FolderBrowserDialog();
+         this.btnSelectTargetDirectory = new System.Windows.Forms.Button();
+         this.lblTargetDirectory = new System.Windows.Forms.Label();
+         this.btnRemoveTags = new System.Windows.Forms.Button();
+         this.SuspendLayout();
+         // 
+         // btnSelectTargetDirectory
+         // 
+         this.btnSelectTargetDirectory.Location = new System.Drawing.Point(426, 5);
+         this.btnSelectTargetDirectory.Name = "btnSelectTargetDirectory";
+         this.btnSelectTargetDirectory.Size = new System.Drawing.Size(75, 35);
+         this.btnSelectTargetDirectory.TabIndex = 0;
+         this.btnSelectTargetDirectory.Text = "Browse";
+         this.btnSelectTargetDirectory.UseVisualStyleBackColor = true;
+         this.btnSelectTargetDirectory.Click += new System.EventHandler(this.btnSelectTargetDirectory_Click);
+         // 
+         // lblTargetDirectory
+         // 
+         this.lblTargetDirectory.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+         this.lblTargetDirectory.Location = new System.Drawing.Point(13, 13);
+         this.lblTargetDirectory.Name = "lblTargetDirectory";
+         this.lblTargetDirectory.Size = new System.Drawing.Size(407, 23);
+         this.lblTargetDirectory.TabIndex = 1;
+         this.lblTargetDirectory.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+         // 
+         // btnRemoveTags
+         // 
+         this.btnRemoveTags.Location = new System.Drawing.Point(210, 60);
+         this.btnRemoveTags.Name = "btnRemoveTags";
+         this.btnRemoveTags.Size = new System.Drawing.Size(98, 23);
+         this.btnRemoveTags.TabIndex = 2;
+         this.btnRemoveTags.Text = "Remove Tags";
+         this.btnRemoveTags.UseVisualStyleBackColor = true;
+         this.btnRemoveTags.Click += new System.EventHandler(this.btnRemoveTags_Click);
+         // 
+         // Form1
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(513, 95);
+         this.Controls.Add(this.btnRemoveTags);
+         this.Controls.Add(this.lblTargetDirectory);
+         this.Controls.Add(this.btnSelectTargetDirectory);
+         this.Name = "Form1";
+         this.Text = "Remove Tags";
+         this.ResumeLayout(false);
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.FolderBrowserDialog fbdTaggedDocument;
+      private System.Windows.Forms.Button btnSelectTargetDirectory;
+      private System.Windows.Forms.Label lblTargetDirectory;
+      private System.Windows.Forms.Button btnRemoveTags;
+   }
+}
+
---
+++ C#/RemoveTags/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Shell;
+
+namespace RemoveTags
+{
+   public partial class Form1 : Form
+   {
+      public Form1()
+      {
+         InitializeComponent();
+      }
+
+      public string targetDirectory;
+
+      private void btnSelectTargetDirectory_Click(object sender, EventArgs e)
+      {
+         fbdTaggedDocument.RootFolder = System.Environment.SpecialFolder.Desktop;
+         fbdTaggedDocument.ShowDialog();
+         this.targetDirectory = fbdTaggedDocument.SelectedPath;
+         lblTargetDirectory.Text = this.targetDirectory;
+      }
+
+      private void btnRemoveTags_Click(object sender, EventArgs e)
+      {
+         string[] allFiles = Directory.GetFiles(this.targetDirectory);
+         List<string> listAllFiles = allFiles.ToList();
+         DialogResult result = MessageBox.Show("Are you sure you want to remove tags from " + listAllFiles.Count + "Files?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+         if (result == DialogResult.Yes)
+         {
+
+            foreach (var i in listAllFiles)
+            {
+               removeTags(i);
+            }
+            MessageBox.Show("Tags Removed!", "Complete");
+         }
+         else
+            MessageBox.Show("Operation Aborted");
+      }
+
+      public void removeTags(string p_Path)
+      {
+         var shellFile = ShellFile.FromFilePath(p_Path);
+         shellFile.Properties.System.Keywords.ClearValue();
+      }
+
+   }
+}
