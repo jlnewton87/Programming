bcbb8b229d2ea81c20794c49da02f79b1df2660b
Joshua Newton <joshnewton@outlook.com>
2012-11-19 10:48:17 -0500

    Adding functionality to both patient and patientlist

--- Java/Android/WaitngPatientSorter/src/com/dracosoftrnd/waitingPatientSorter/Patient.java
+++ Java/Android/WaitngPatientSorter/src/com/dracosoftrnd/waitingPatientSorter/Patient.java
@@ -3,8 +3,20 @@ package com.dracosoftrnd.waitingPatientSorter;
 import java.util.Date;
 
 public class Patient {
+	
+	public String firstName;
+	public String lastName;
+	public Date appointmentTime;
+	public Date checkinTime;
+	public Date effectiveTime;
+	
+	
 	public Patient(String fName, String lName, Date appointmentTime, Date checkinTime, Date effectiveTime)
 	{
-		
+		this.firstName = fName;
+		this.lastName = lName;
+		this.appointmentTime = appointmentTime;
+		this.checkinTime = checkinTime;
+		this.effectiveTime = effectiveTime;
 	}
 }
--- Java/Android/WaitngPatientSorter/src/com/dracosoftrnd/waitingPatientSorter/PatientList.java
+++ Java/Android/WaitngPatientSorter/src/com/dracosoftrnd/waitingPatientSorter/PatientList.java
@@ -1,5 +1,14 @@
 package com.dracosoftrnd.waitingPatientSorter;
 
+import java.util.*;
+
 public class PatientList {
+	
+	public ArrayList<Patient> patients;
+	
+	public void addPatient(Patient newPatient)
+	{
+		patients.add(newPatient);
+	}
 
 }
