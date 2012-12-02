6d2a6c784ee69eef8a858e4272a063d4a5f439d1
Joshua Newton <joshnewton@outlook.com>
2012-12-01 23:12:22 -0500

    Adding form to add patients, and some functionality

--- Java/Android/WaitngPatientSorter/src/com/dracosoftrnd/waitingPatientSorter/PatientListActivity.java
+++ Java/Android/WaitngPatientSorter/src/com/dracosoftrnd/waitingPatientSorter/PatientListActivity.java
@@ -5,7 +5,9 @@ import java.util.ArrayList;
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.ListActivity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -29,5 +31,17 @@ public class PatientListActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.patient_list, menu);
         return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+ 
+        switch (item.getItemId())
+        {
+        case R.id.add_patient:
+            Intent addPatient = new Intent(getApplicationContext(), AddPatientActivity.class);
+            startActivity(addPatient);
+            return true;
+        }
+		return false;
     }
 }
