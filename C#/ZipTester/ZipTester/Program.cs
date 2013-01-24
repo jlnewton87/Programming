0b099b88b4f2ce740126f09890cbbf1257aa3e80
Joshua Newton <joshnewton@outlook.com>
2013-01-23 21:58:32 -0500

    Adding example project using DotNetZip to create a zip archive while taking a directory as input

---
+++ C#/ZipTester/ZipTester/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ionic.Zip;
+
+namespace ZipTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string directory = "C:\\Users\\josh\\Documents\\GitHub\\Programming\\HTML\\3260";
+            ZipFile zip = new ZipFile();
+            zip.AddDirectory(directory);
+            zip.Save(directory + "\\3260.zip");
+        }
+    }
+}
