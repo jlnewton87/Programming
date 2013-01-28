fb8b9b38b08a64b27c5b3c7c3f48ad5e927467ff
Joshua Newton <joshnewton@outlook.com>
2013-01-28 18:56:15 -0500

    Adding completed agecount function.  This passed the test, but does not effectivly filter with ages < 10

--- R/agecount.R
+++ R/agecount.R
@@ -1,7 +1,11 @@
 agecount <- function(age = NULL) {
   ## Check that "age" is non-NULL; else throw error
+  if(length(age) == 0){stop("You must supply an age")}
   ## Read "homicides.txt" data file
-  ## Extract ages of victims; ignore records where no age is
-  ## given
+  homicides <- readLines("homicides.txt")
+  ## Extract ages of victims; ignore records where no age is given
+  ageRegEx <- paste(as.character(age), " years old", sep="")
+  i <- grep(ageRegEx, homicides)
   ## Return integer containing count of homicides for that age
+  return(length(i))
 }
