80e1b09ca7ba625ed2ea9a85973f52cffd7dfb73
Joshua Newton <joshnewton@outlook.com>
2013-01-28 16:24:10 -0500

    commit of function shell for agecount function(Part 2 of programming assignment 4

---
+++ R/agecount.R
@@ -0,0 +1,7 @@
+agecount <- function(age = NULL) {
+  ## Check that "age" is non-NULL; else throw error
+  ## Read "homicides.txt" data file
+  ## Extract ages of victims; ignore records where no age is
+  ## given
+  ## Return integer containing count of homicides for that age
+}
