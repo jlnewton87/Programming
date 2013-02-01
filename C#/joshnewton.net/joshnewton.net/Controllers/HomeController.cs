999a05729bcf7462fa7f65200ef926f11c921163
Josh Newton <jnewton@cmiresearch.com>
2013-02-01 12:36:28 -0500

    initial commit of joshnewton.net, and adding the GoDaddy start page in case it comes in use later on.

---
+++ C#/joshnewton.net/joshnewton.net/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace joshnewton.net.Controllers
+{
+   public class HomeController : Controller
+   {
+      public ActionResult Index()
+      {
+         ViewBag.Message = "Where it all begins.";
+
+         return View();
+      }
+
+      public ActionResult About()
+      {
+         ViewBag.Message = "Ideas.";
+
+         return View();
+      }
+
+      public ActionResult Contact()
+      {
+         ViewBag.Message = "Contact me.";
+
+         return View();
+      }
+   }
+}
