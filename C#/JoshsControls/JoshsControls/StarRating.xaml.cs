f3a4595aec32c92f67f82488f0bd7299e7cedcf6
Josh Newton <jnewton@cmiresearch.com>
2013-02-14 16:55:00 -0500

    start of custome=e control project

---
+++ C#/JoshsControls/JoshsControls/StarRating.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace JoshsControls
+{
+   public sealed partial class StarRating : UserControl
+   {
+      private double val;
+      public event EventHandler ValueChanged;
+      public double Value
+      {
+         get
+         {
+            return val;
+         }
+         set
+         {
+            val = value;
+            GradientStopCollection col = new GradientStopCollection();
+            GradientStop stop1 = new GradientStop();
+            GradientStop stop2 = new GradientStop();
+            stop1.Color = Color.FromArgb(255, 255, 215, 0);
+            stop1.Offset = val / 5;
+            stop2.Color = Color.FromArgb(255, 0, 0, 0);
+            stop2.Offset = val / 5;
+            col.Add(stop1);
+            col.Add(stop2);
+            LinearGradientBrush brush = new LinearGradientBrush(col, 0);
+            Stars.Fill = brush;
+            OnValueChanged();
+         }
+      }
+
+      private void OnValueChanged()
+      {
+         EventHandler localEvent = ValueChanged;
+         if (localEvent != null)
+         {
+            localEvent(this, EventArgs.Empty);
+         }
+      }
+
+      public StarRating()
+      {
+         this.InitializeComponent();
+      }
+   }
+}
---
+++ C#/JoshsControls/JoshsControls/WaterMarkedCheckBox.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace JoshsControls
+{
+   public sealed partial class WaterMarkedCheckBox : UserControl
+   {
+      private string wm = "Water Mark";
+      public string Text
+      {
+         get{return WMTB.Text;}
+         set{WMTB.Text = value;}
+      }
+      public string WaterMark
+      {
+         get{return wm;}
+         set
+         {
+            wm = value;
+         }
+      }
+
+      public WaterMarkedCheckBox()
+      {
+         this.InitializeComponent();
+         
+      }
+
+      private void RemoveWaterMark(object sender, RoutedEventArgs e)
+      {
+         WMTB.Text = "";
+         WMTB.Foreground = new SolidColorBrush(Colors.Black);
+         WMTB.Background = new SolidColorBrush(Colors.White);
+      }
+
+      private void CheckIfWaterMarkNeeded(object sender, RoutedEventArgs e)
+      {
+         if (WMTB.Text == "")
+         {
+            WMTB.Foreground = new SolidColorBrush(Colors.DarkGray);
+            WMTB.Text = wm;
+         }
+         else
+         {
+            WMTB.Background = new SolidColorBrush(Colors.White);
+         }
+      }
+   }
+}
