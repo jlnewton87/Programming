806c0321c386c79695d5d6d319c752c687c26270
Josh Newton <jnewton@cmiresearch.com>
2013-08-29 15:36:43 -0400

    working db functionality for test chat project

--- C#/ChatV1/ChatV1/Controllers/HomeController.cs
+++ C#/ChatV1/ChatV1/Controllers/HomeController.cs
@@ -3,15 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ChatV1.Models;
 
 namespace ChatV1.Controllers
 {
     public class HomeController : Controller
     {
 
+        List<Message> messages;
+        int messageIndex = 1;
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public string AddMessage(string jsonMessage)
+        {
+            string response = jsonMessage;
+            using (MessageContext context = new MessageContext())
+            {
+                context.Messages.Add(new Message
+                {
+                    Text = jsonMessage
+                });
+                context.SaveChanges();
+            }
+            return response;
         }
 
     }
---
+++ C#/ChatV1/ChatV1/Models/MessageDB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ChatV1.Models
+{
+    public class Message
+    {
+        public int Id{get;set;}
+        public string Text{get;set;}
+    }
+
+    public class MessageContext : DbContext
+    {
+        public DbSet<Message> Messages {get;set;}
+    }
+}
