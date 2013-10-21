fe1a039919766f35bab57a7dfbb7adf1f823e3cb
Joshua Newton <joshnewton@outook.com>
2013-10-20 20:02:10 -0400

    Adding grad announcement/RSVP app 2

---
+++ C#/joshnewton.net/joshnewton.net/Content/scripts/Grad_Rsvp.js
@@ -0,0 +1,72 @@
+﻿var comingDiv;
+var notComingDiv;
+$(document).ready(function () {
+    comingDiv = document.getElementById("yay");
+    notComingDiv = document.getElementById("nay");
+
+    $("#coming").click(function() {
+        $(comingDiv).dialog({
+            modal: true,
+            width:"600px",
+            title: "RSVP",
+            buttons: {
+                "Submit": function () {
+                    $("button").attr('disabled','disabled');
+                    var data = {
+                        NumInParty: $("#numInParty").val(),
+                        Message: $("#messageToGradComing").val(),
+                        AttendeeId: $("#attendeeId").val(),
+                        willCome:true
+                    };
+                    $.ajax({
+                        complete: function () {
+                            $("button").removeAttr('disabled');
+                        },
+                        url: $("#postURL").val(),
+                        data: data,
+                        method: "POST"
+                    }).done(function(){
+                        window.location.href = $("#redirectUrl").val();
+                    });
+                },
+                Cancel: function () {
+                    $(this).dialog("close");
+                }
+            }
+        });
+    });
+
+    $("#notComing").click(function() {
+        $(notComingDiv).dialog({
+            modal: true,
+            width: "600px",
+            title: "RSVP",
+            buttons: {
+                "Submit": function () {
+                    $("button").attr('disabled', 'disabled');
+                    var data = {
+                        NumInParty: 0,
+                        Message: $("#messageToGradNotComing").val(),
+                        AttendeeId: $("#attendeeId").val(),
+                        willCome: false
+                    };
+                    $.ajax({
+                        complete: function () {
+                            $("button").removeAttr('disabled');
+                        },
+                        url: $("#postURL").val(),
+                        data: data,
+                        method: "POST"
+                    }).done(function () {
+                        window.location.href = $("#redirectUrl").val();
+                    });
+                },
+                Cancel: function () {
+                    $(this).dialog("close");
+                }
+            }
+        });
+    });
+
+    }
+);
---
+++ C#/joshnewton.net/joshnewton.net/Controllers/GraduationController.cs
@@ -0,0 +1,43 @@
+﻿using joshnewton.net.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace joshnewton.net.Controllers
+{
+    public class GraduationController : Controller
+    {
+        //
+        // GET: /Graduation/
+
+        public ActionResult RSVP(string Id)
+        {
+            Attendee RsvpAttendee = new Attendee();
+            using (GraduationEntities ents = new GraduationEntities())
+            {
+                RsvpAttendee = ents.Attendees.Where(x => x.UID == Id).Single();
+            }
+            return View("Index", RsvpAttendee);
+        }
+
+        [HttpPost]
+        public void SubmitRsvp(RSVP RsvpData)
+        {
+            Attendee person = new Attendee();
+            using (GraduationEntities ents = new GraduationEntities())
+            {
+                ents.RSVPs.Add(RsvpData);
+                person = ents.Attendees.Where(x => x.Id == RsvpData.AttendeeId).Single();
+                ents.SaveChanges();
+            }
+        }
+
+        public ActionResult Thanks()
+        {
+            return View();
+        }
+
+    }
+}
