ae97b07fa5e9167ee1f4892c085379553025517a
Joshua Newton <joshnewton@outook.com>
2013-10-28 21:28:15 -0400

    Update to Grad RSVP, and adding data-dump site hosted on Azure

---
+++ C#/EventDataService/Controllers/GradEventController.cs
@@ -0,0 +1,26 @@
+﻿using EventDataService.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EventDataService.Controllers
+{
+    public class GradEventController : Controller
+    {
+
+        public string GetData()
+        {
+            string data;
+            using (GraduationEntities ents = new GraduationEntities())
+            {
+                data = JsonConvert.SerializeObject(ents.Details.ToArray());
+            }
+            return data;
+        }
+
+    }
+}
---
+++ C#/RSVP Details/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using RSVP_Details.Resources;
+
+namespace RSVP_Details
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
